e6d801f1330248886900e3db89893fba3f31cd39
Radoslaw Kaczybura <radoslaw.kaczybura@gmail.com>
2025-05-22 19:45:13 +0200

    Initial commit backend

---
+++ users/views.py
@@ -0,0 +1,30 @@
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
+# Rejestracja
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Informacje o zalogowanym użytkowniku
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def buy_premium(request):
+    user = request.user
+    user.is_premium = True
+    user.save()
+    return Response({"message": "Dostęp premium przyznany!"})
