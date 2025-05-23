43b27fa1208b6c55c316b2fa08dfc1ed8a5e2997
Radoslaw Kaczybura <radoslaw.kaczybura@gmail.com>
2025-05-23 21:58:59 +0200

    Test

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics, permissions
 from .models import User
 from .serializers import RegisterSerializer, UserSerializer
-from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 # Rejestracja
